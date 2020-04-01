ba5e31663ed7fc3dba1c6ca7046fd7f26c933117
matias chattellino <matiaschattellino@gmail.com>
2020-04-01 20:57:52 -0300

    custom buttom and form

---
+++ crwn-clothing/src/pages/sign-ind-and-sign-up/sign-ind-and-sign-up.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/sign-in.component';
+
+import './sign-ind-and-sign-up.styles.scss';
+
+const SignInAndSignUpPage = () => (
+    <div classname="sign-in-and-sign-up">
+        <SignIn />
+    </div>
+);
+
+export default SignInAndSignUpPage;
