ba4587ad96fd3aba55c8802c3d8bca9708d6d340
matias chattellino <matiaschattellino@gmail.com>
2020-04-03 20:55:48 -0300

    finished sign in and sign up

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import SignInAndSignUpPage from './pages/sign-ind-and-sign-up/sign-ind-and-sign-up.component'; 
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 
 class App extends React.Component {
@@ -21,9 +21,22 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
   
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user});
-    })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+        if (userAuth) {
+          const userRef = await createUserProfileDocument(userAuth);
+
+          userRef.onSnapshot(snapShot => {
+               this.setState({
+                 currentUser: {
+                   id: snapShot.id,
+                   ...snapShot.data()
+                 }
+               });
+          });
+        }
+        this.setState({currentUser: userAuth})
+    });
+
   }
 
   componentWillUnmount() {
